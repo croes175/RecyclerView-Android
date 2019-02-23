33190f723dd0ca4144fe0248049b74cef50fdfe9
croes175 <croesgomez@gmail.com>
2019-02-22 21:47:53 -0400

    prueba 2

--- app/src/main/java/com/androiddesdecero/reciclerview/MainActivity.java
+++ app/src/main/java/com/androiddesdecero/reciclerview/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView listaPesos;
     private PesoAdaptador adaptador;
 
-
+/*travieso */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
